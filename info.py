ce80786b9183d2a34f309c58c15667491d8c07d8
WEREWOLF-DEMON <153902241+WEREWOLF-DEMON@users.noreply.github.com>
2024-10-12 15:27:40 +0530

    info.py

--- info.py
+++ info.py
@@ -10,7 +10,7 @@ class Config(object):
     # Other Configs
     BOT_START_TIME = time.time()
     OWNER    = int(os.environ.get("OWNER", "6656608288"))  # ⚠️ Required
-    SUDO = list(map(int, os.environ.get("SUDO", "6656608288"8156009521"6965059812").split()))  # ⚠️ Required
+    SUDO = list(map(int, os.environ.get("SUDO", "6656608288 8156009521 6965059812").split()))  # ⚠️ Required
     # Web Response Config
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
     PORT = int(os.environ.get("PORT", "8080"))
